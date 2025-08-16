46e379444662f4968e52f2ebd47714d349269bf5
김태현 <eedcs@naver.com>
2025-08-16 12:33:08 +0900

    java test

---
+++ src/main/java/org/example/Head01_JavaProgrammingStart/example08/ArrayLengthExample.java
@@ -0,0 +1,8 @@
+package org.example.Head01_JavaProgrammingStart.example08;
+
+public class ArrayLengthExample {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        System.out.println(nums.length); // 3 ,일차배열.length는 행길이갯수,이차배열.length는 행개수
+    }
+}
