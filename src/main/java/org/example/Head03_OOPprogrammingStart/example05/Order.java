fba3da1cc85d8d76b00d8969c681977862318812
김태현 <eedcs@naver.com>
2025-08-17 12:31:42 +0900

    java test

--- src/main/java/org/example/Head03_OOPprogrammingStart/example05/Order.java
+++ src/main/java/org/example/Head03_OOPprogrammingStart/example05/Order.java
@@ -1,12 +1,18 @@
 package org.example.Head03_OOPprogrammingStart.example05;
 
+// 추상클렉스 선언
 public class Order {
+    // 필드선언
     private String orderId;
     private Delivery delivery;  // Order는 Delivery를 포함(Has-A)
-
+    // 생성자 생성
     public Order(String orderId, String trackingNumber, String carrier) {
+        // 생성자 이름은 클렉스이름(=Order)과 동일하고 반환값은 없다, () 를 매개변수로 받는 생성자 Order를 만든다
+        // String타입의 oderId,trackingNumber,carrier 3가지 객체를 매개변수로 받아라
         this.orderId = orderId;
-        this.delivery = new Delivery(trackingNumber, carrier);  // 내부에서 직접 생성
+        // 현재 클레스에 ordeId필드에 외부 orderId값을 할당해라
+        this.delivery = new Delivery(trackingNumber, carrier);
+        // 현재 클레스에 delivery필드에, 새로운 Delivery(매개변수)객체 인자로 받도록 새로 정의함
     }
 
     public void startOrderDelivery() {
