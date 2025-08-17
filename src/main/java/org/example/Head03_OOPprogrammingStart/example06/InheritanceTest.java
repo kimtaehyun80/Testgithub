e39db803130d130811df1dfb72132acad5030f1b
김태현 <eedcs@naver.com>
2025-08-17 15:33:23 +0900

    java test

--- src/main/java/org/example/Head03_OOPprogrammingStart/example06/InheritanceTest.java
+++ src/main/java/org/example/Head03_OOPprogrammingStart/example06/InheritanceTest.java
@@ -3,8 +3,9 @@ package org.example.Head03_OOPprogrammingStart.example06;
 public class InheritanceTest {
     public static void main(String[] args) {
         User u = new PersonalUser("U001", "Alice", "alice@example.com");
+        //User타입 u변수를 만드는데 PersonalUser()매소드값을 할당해라. 부모클레스User 와 자식클레스PersonalUser 관계 일때 성립
         User w = new BusinessUser("w002", "Challi", "challi@intel.com", "Intel");
-        u.printUserInfo();
+        u.printUserInfo();  // u변수에 부모클레스 변수인 printUserInfo()메서드를 호출해라
         w.printUserInfo();
     }
 }
