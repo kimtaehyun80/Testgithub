e525c97785a792ca4cc83ed0a19e77139b6d6784
김태현 <eedcs@naver.com>
2025-08-17 09:51:05 +0900

    java test

--- src/main/java/org/example/Head03_OOPprogrammingStart/example01/CreditCardPayment.java
+++ src/main/java/org/example/Head03_OOPprogrammingStart/example01/CreditCardPayment.java
@@ -3,6 +3,7 @@ package org.example.Head03_OOPprogrammingStart.example01;
 public class CreditCardPayment implements Payment {
     //임플러먼트는 페이먼트를 상속
     @Override
+    // 오버라이드는 부모클레스 메서드를 자식클레스가 상속해서 동일시그니처(메서드이름,반환타입,매개변수목록)로 재정의하여 구현한다
     public void pay(double amount) {
         // 신용카드 결제에 필요한 로직
         System.out.println("신용카드로 " + amount + "원 결제 완료.");
--- src/main/java/org/example/Head03_OOPprogrammingStart/example02/AbstractPayment.java
+++ src/main/java/org/example/Head03_OOPprogrammingStart/example02/AbstractPayment.java
@@ -1,9 +1,9 @@
 package org.example.Head03_OOPprogrammingStart.example02;
 
-public abstract class AbstractPayment {
+public abstract class AbstractPayment {   // 추상클레스
     // 공통 검증 로직
     protected void validatePayment(double amount) {
-        if (amount <= 0) {
+        if (amount <= 0) {  // amount변수가 0이하일때 참이면 예외발생, illegalargumentexception()은 예외발생 클레스로()내용 보여줌
             throw new IllegalArgumentException("결제 금액이 유효하지 않습니다.");
         }
     }
@@ -15,9 +15,12 @@ public abstract class AbstractPayment {
         }
     }
 
-    // 추상 메소드: 구체 클래스가 구현해야 함
+
     public abstract void pay(double amount);
+    // pay라는 이름의 추상메서드 구현코드, abstract는 구현체 없이 추상클레스 나 인터페이스에만 정의 된다는것을 의미함.매개변수는 amount
+    // 구현체(=main) 가 없기에 상속받은 하위클레스에서 반드시 구현 해야된다.
     public abstract void cancel(double amount);
+    //cancel이라는 추상메서드이고 amount라는 매개변수를 갖는다.
 }
 
 
