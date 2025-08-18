0f38d70948536336289f8fe6729e80058d4298fa
김태현 <eedcs@naver.com>
2025-08-18 20:55:47 +0900

    java test

--- src/main/java/org/example/Head04_JCF/example03/LazyEvaluationExample.java
+++ src/main/java/org/example/Head04_JCF/example03/LazyEvaluationExample.java
@@ -7,10 +7,10 @@ public class LazyEvaluationExample {
         List<String> names = List.of("Alice", "Bob", "Charlie", "David");
 
         System.out.println("스트림 생성 및 중간 연산 정의");
-        var stream = names.stream()
+        var stream = names.stream()  // var지역변수추론타입(자동추론)stream변수선언
                 .filter(name -> {
                     System.out.println("filter 호출: " + name);
-                    return name.length() > 3;
+                    return name.length() > 3;  // 참,거짓 반환
                 });
 
         System.out.println("아직 아무것도 실행되지 않음!");
