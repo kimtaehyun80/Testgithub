998cd833dc40e22f1163e93f1ccb49c73ce99aea
김태현 <eedcs@naver.com>
2025-08-18 20:49:18 +0900

    java test

--- src/main/java/org/example/Head04_JCF/example03/StreamBasicExample.java
+++ src/main/java/org/example/Head04_JCF/example03/StreamBasicExample.java
@@ -6,10 +6,14 @@ import java.util.List;
 public class StreamBasicExample {
     public static void main(String[] args) {
         List<String> names = Arrays.asList("Tom", "Jerry", "Kim", "Tommy");
+// Arrays.asList()는 배열을 리스트 객체로 변환해라, 그리고 names객체에 할당해라
+                 names.stream()                     // nsmes객체이 Stream 생성
+                .filter(name -> name.startsWith("T"))
+                         //중간연산1, 이름에서->t로시작하는 문자열을 필터해라
+                .map(String::toUpperCase)
+                         //중간연산2, 입력된 문자열을 대문자로 변환해라
+                .forEach(System.out::println);
+                 //최종연산, 컬렉션 각 항목을 System.out객체의println 메소드 호출해서출력
 
-        names.stream()                          // Stream 생성
-                .filter(name -> name.startsWith("T")) // 중간 연산 1
-                .map(String::toUpperCase)           // 중간 연산 2
-                .forEach(System.out::println);      // 최종 연산
     }
 }
