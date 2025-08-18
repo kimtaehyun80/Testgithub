20d4c8c5ba0fe23563794bc6ed4e38f25b394120
김태현 <eedcs@naver.com>
2025-08-18 21:49:18 +0900

    java test

--- src/main/java/org/example/Head04_JCF/example03/MapNamesExample.java
+++ src/main/java/org/example/Head04_JCF/example03/MapNamesExample.java
@@ -9,14 +9,16 @@ public class MapNamesExample {
         List<String> names = Arrays.asList("Alice", "Bob", "Charlie");
 
         Function<String, Integer> nameLength = new Function<String, Integer>() {
+            // new 인터페이스() { ... } → 무조건 익명 클래스
             @Override
             public Integer apply(String s) {
                 return s.length();
             }
         };
 
-        names.stream()
+                 names.stream()
                 .map(nameLength)
+                         // .map(nameLength)는 문자길이 생성,stream api 메소드
                 .forEach(length -> System.out.println("Name length: " + length));
     }
 }
