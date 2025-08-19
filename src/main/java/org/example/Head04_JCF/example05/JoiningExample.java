13cfb34e9debfaa95c4d0cf85056e964af0528ec
김태현 <eedcs@naver.com>
2025-08-19 09:31:56 +0900

    java test

---
+++ src/main/java/org/example/Head04_JCF/example05/JoiningExample.java
@@ -0,0 +1,17 @@
+package org.example.Head04_JCF.example05;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class JoiningExample {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Alice", "Bob", "Charlie");
+
+        String result = names.stream()
+                .collect(Collectors.joining(", ")); // 구분자 쉼표
+
+        System.out.println("Formatted result: " + result);
+    }
+}
+
---
+++ src/main/java/org/example/Head04_JCF/example05/ToSetExample.java
@@ -0,0 +1,18 @@
+package org.example.Head04_JCF.example05;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class ToSetExample {
+    public static void main(String[] args) {
+        List<String> fruits = Arrays.asList("Apple", "Banana", "Apple", "Cherry", "Banana");
+
+        Set<String> uniqueFruits = fruits.stream()
+                .collect(Collectors.toSet());
+
+        System.out.println("Unique fruits: " + uniqueFruits);
+    }
+}
+
