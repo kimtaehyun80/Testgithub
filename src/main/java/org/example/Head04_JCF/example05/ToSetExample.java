6be9192029f1b05f01cc873726f4a1f393ddf89b
김태현 <eedcs@naver.com>
2025-08-19 12:35:18 +0900

    java test

--- src/main/java/org/example/Head04_JCF/example05/ToSetExample.java
+++ src/main/java/org/example/Head04_JCF/example05/ToSetExample.java
@@ -10,7 +10,7 @@ public class ToSetExample {
         List<String> fruits = Arrays.asList("Apple", "Banana", "Apple", "Cherry", "Banana");
 
         Set<String> uniqueFruits = fruits.stream()
-                .collect(Collectors.toSet());
+                .collect(Collectors.toSet());  // otSet으로 중복문자열 수집(중복만 출력x,중복은 하나로
 
         System.out.println("Unique fruits: " + uniqueFruits);
     }
