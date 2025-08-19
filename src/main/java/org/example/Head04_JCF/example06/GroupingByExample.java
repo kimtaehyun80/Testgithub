1437e4a7d3af79319b123c691c3ff8e89bc12a00
김태현 <eedcs@naver.com>
2025-08-19 21:56:08 +0900

    java test

--- src/main/java/org/example/Head04_JCF/example06/GroupingByExample.java
+++ src/main/java/org/example/Head04_JCF/example06/GroupingByExample.java
@@ -11,7 +11,10 @@ public class GroupingByExample {
 
         // 이름의 첫 글자를 기준으로 그룹화
         Map<Character, List<String>> grouped = names.stream()
+                //캐릭터 타입키와 문자열 리스트값을 갖는 맵타입 grouped변수에
+                // names변수값을 스트림한것을  할당해라
                 .collect(Collectors.groupingBy(name -> name.charAt(0)));
+        //문자열 리스트 첫글라를 기준으로 그룹화 해라
 
         System.out.println(grouped);
     }
